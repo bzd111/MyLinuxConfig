8c83a1c360a3f89eae997e3e793a8fbc4162cdf9
bzd111 <1240450484@qq.com>
2017-04-07 08:43:43 +0000

    first success running

--- centos_install.sh
+++ centos_install.sh
@@ -16,20 +16,24 @@ yum install  -y  ctags vim curl tmux  zsh git httpie ssh make lrzsz
 # install zsh
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 chsh -s /bin/zsh
-cp ./.centos_zshrc  ~/.zshrc
+cp $(pwd)/centos_zshrc  ~/.zshrc
 
 # install autojump
-yum  install -y autojump
-cd ~
-echo ". /usr/share/autojump/autojump.sh" >> .zshrc
+#yum  install -y autojump
+#cd ~
+#echo ". /usr/share/autojump/autojump.sh" >> .zshrc
+git clone git://github.com/joelthelion/autojump.git /root/autojump
+cd /root/autojump
+./install.py
 
+cd - 
 # install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp ./.cenots_vimrc ~/.vimrc
+cp $(pwd)/centos_vimrc ~/.vimrc
 vim +PluginInstall +qall
 
 # config tmux
-cp ./.centos_tmux.conf ~/.tmux.conf
+cp $(pwd)/centos_tmux.conf ~/.tmux.conf
 tmux kill-server
 
 # restart
