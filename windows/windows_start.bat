164a1482746a176fdec253c66c70ba1caa69e821
zhangcheng <zhangcheng@broada.com>
2017-05-08 10:00:39 +0800

    add start windows bat

---
+++ windows/windows_start.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Starting 桌面整理
+start  "" "D:\program files (x86)\cmcm\kdesk\kdesk64.exe" 
+echo Starting Launch
+start  "" "D:\Launchy\Launchy.exe"
+echo Starting Evernote
+start  "" "D:\Evernote\Evernote.exe"
+echo Starting 有道词典
+start  "" "D:\Youdao\Dict\YodaoDict.exe"
+echo Starting 网易云音乐
+start  "" "D:\Netease\CloudMusic\cloudmusic.exe"
+REM start /wait "" "path"
+exit
