e4647be50d27a25063e382d8d575b894f675a400
zhangcheng <zhangcheng@uyunsoft.cn>
2017-06-24 10:34:58 +0800

    add bat file of start in company

---
+++ windows/windows_start_work.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Starting 桌面整理
+start  "" "D:\program files (x86)\cmcm\kdesk\kdesk64.exe" 
+echo Starting Launch
+start  "" "D:\Launchy\Launchy.exe"
+echo Starting Evernote
+start  "" "D:\Evernote\Evernote.exe"
+echo Starting 有道词典
+start  "" "D:\Program Files (x86)\Dict\YodaoDict.exe"
+echo Starting 网易云音乐
+start  "" "D:\Netease\CloudMusic\cloudmusic.exe"
+echo Starting Google
+start  "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+REM start /wait "" "path"
+exit
