a00dad7939b85bd0568ad9c23c5736160a849232
zhangcheng <zhangcheng@uyunsoft.cn>
2017-08-06 15:50:58 +0800

    modify tmux conf

--- centos/centos_install.sh
+++ centos/centos_install.sh
@@ -9,8 +9,10 @@ yum makecache
 # now install docker
 curl -sSL https://get.daocloud.io/docker | sh
 curl -L https://get.daocloud.io/docker/compose/releases/download/1.12.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://a7dbd8ae.m.daocloud.io
 chmod +x /usr/local/bin/docker-compose
 systemctl enable docker
+systemctl restart docker
 
 # now start install
 yum install -y python-devel mysql-devel
