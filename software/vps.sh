9c472e86c8222054adc226c956e8ba62a5d205e8
bzd111 <1240450484@qq.com>
2018-05-07 23:25:56 +0800

    add vps.sh to setup ssr

---
+++ software/vps.sh
@@ -0,0 +1,15 @@
+#!/usr/bin bash
+# 前期准备
+yum -y install python-setuptools
+easy_install pip
+pip install shadowsocks
+
+# 启动ss
+ssserver -p 8388 -k fangqiang -m rc4-md5 -d start 
+
+# 开放8388端口,重启防火墙
+firewall-cmd --zone=public --add-port=8388/tcp --permanent
+systemctl restart firewalld
+
+# 开机启动
+echo "ssserver -p 8388 -k fangqiang -m rc4-md5 -d start" >> /etc/rc.local
